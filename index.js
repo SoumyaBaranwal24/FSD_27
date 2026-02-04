80088cd2c5ed68f7226fd3e521710a4671a17da4
SoumyaBaranwal24 <soumyabaranwal62@gmail.com>
2026-02-04 15:29:53 +0530

    First commit of JS code

---
+++ index.js
@@ -0,0 +1,27 @@
+let a = 10;
+let b = 20;
+let sum = a+b;
+console.log("the sum of a and b:" + sum);
+let bool = true;
+
+let und;
+let nul=null;
+
+console.log(typeof bool);
+console.log(typeof und);
+console.log(typeof nul);
+
+console.log(a*b);
+console.log(a/b);
+console.log(a-b);
+console.log(a%b);
+
+
+
+let age = 20;
+if(age>=18){
+    console.log("You are eligible to vote");
+}
+else{
+    console.log("you are not");
+}
