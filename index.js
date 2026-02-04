4b569ce6c76eba1b2e42ae18a49bfddae6419352
SoumyaBaranwal24 <soumyabaranwal62@gmail.com>
2026-02-04 15:54:54 +0530

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// let const var
 let a = 10;
 let b = 20;
 let sum = a+b;
@@ -25,3 +26,59 @@ if(age>=18){
 else{
     console.log("you are not");
 }
+
+//array
+
+let arr = [18,26,6,7,8];
+
+console.log(arr[3]);
+
+arr.push(10);
+arr.pop(6);
+
+console.log(arr.length)
+
+//object
+
+let students = {
+    name:"radha",
+    age:32
+
+}
+
+//function
+
+function add(p,q){
+    return p+q;
+}
+
+console.log(add(7,8));
+
+
+//arrow function
+// ingle line codes by arrow function
+const multiply = (p,q) => p*q;
+
+console.log("arrow function",multiply(5,15));
+
+//date
+let today = new Date();
+console.log(today);
+
+
+//math
+console.log(Math.sqrt(36));
+console.log(Math.random());
+
+//string
+
+txt = "hello hiiiii";
+
+console.log(txt.length);
+console.log(txt.toUpperCase());//error
+
+//type conversion
+
+let strum="123";
+let convert = Number(strum);
+console.log(convert+10);
